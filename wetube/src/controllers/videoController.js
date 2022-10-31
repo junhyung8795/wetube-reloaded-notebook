f76a6cfb2cae6c4a9ae5e666ff08c02a0be00eaa
junhyung8795 <junhyung8795@naver.com>
2022-10-31 17:50:09 +0900

    how 2 add html easily using "pug"
    When you have to add html element using express,
    we must use pug. Pug is the converter which can make simple text to real html. First, See index.js and it says, app.set("views", process.cwd() + "/src/views");
    . By default, when express activate pug, express will find pug file in the /views folder. But, express find the /views folder at Current Working Directory. CWD's standard is where the server started. It starts from "package.json". So, wetube folder is the CWD. Actually it would find the views folder like this.
    ...document/wetube/views
    But I wanna put the /views folder into /src floder(because it is the source of the project also).
    index.js line 12 wil change the default path to /src/views. Second thing is Pug can simplify pug files.
    Focus on base.pug and the other pug files.
    base.pug is like a dough of pizza. It is the base of other html pages. And as you can see there are "include" and "block". We can use "include" by making other folder called "partials" ,adding footer.pug and writing "include partials/footer.pug" at the base.pug. Additionally, we can use "extends" the "base.pug" using "block". Writing down "block" in the file means "I'll change this part in the other file for example 
    "block content 
        h1 Wetube"

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,13 +1,8 @@
-export const trending = (req, res) => res.send("Homepage Videos!");
+export const trending = (req, res) => res.render("home");
 
-export const see =(req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Videos ID:${req.params.id}`);
-};
-export const edit =(req, res) => {
-    console.log(req.params);
-    return res.send("Edit");
-};
+export const see =(req, res) => res.render("watch");
+export const edit =(req, res) => res.render("edit");
+
 export const search =(req, res) => res.send("Search");
 export const upload =(req, res) => res.send("Upload Video");
 export const deleteVideo =(req, res) => {
