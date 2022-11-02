4bc2a320f62e57de13a9484a13b75e7b09081e74
junhyung8795 <junhyung8795@naver.com>
2022-11-02 19:02:22 +0900

    Method POST(1)
    POST Method is used to send file or data that change something in proposed url. So editing is a changing some data. So we must use POST. Additionally, there is "action". "action" is the url which you want to send data to. We can write like
    form(action="/somewhere").In this case if you submit, url will be come "localhost:4000/somewhere".
    Whatever.. Express would not understand the POST until you make videoRouter.post(). So let's see the Video Router.Line9+, videoRouter.post("/:id(\\d+)/edit", postEdit).
    In addition, "edit" was modified to "getEdit".
    And then, "postEdit" was added to post router.
    Remember, POST method is used for modifying database by sending data. If you use GET method, and name="title", form will change the url like
    /videos/12/edit?title=lalalla. GET is just getting data.
    POST is mofying the database using that data.

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -35,13 +35,16 @@ export const watch =(req, res) => {
     /*const { id }= req.param도 똑같은 의미이다.*/ 
     const video = videos[id-1];/*id를 통해 video를 받는다.
     라우터애서 id를 받고-> 그 아이디를 request정보로 받고 -> 그 id로 video가 어떤애인지 배열에서 찾고 저장하는것*/
-    return res.render("watch", {pageTitle: `Watching ${video.title}`, video:video /*video라고만써도됨 video라는 오브젝트 그대로 보낸다는 의미*/});
+    return res.render("watch", {pageTitle: `Watching: ${video.title}`, video:video/*video라고만써도됨 video라는 오브젝트 그대로 보낸다는 의미*/});
 }
-export const edit =(req, res) => res.render("edit");
+export const getEdit =(req, res) => {
+    const {id}= req.params;
+    const video = videos[id-1];
+    return res.render("edit", {pageTitle : `Editing: ${video.title}`, video});
+}
 
-export const search =(req, res) => res.send("Search");
-export const upload =(req, res) => res.send("Upload Video");
-export const deleteVideo =(req, res) => {
-    console.log(req.params);
-    return res.send("Delete Video");
+export const postEdit= (req, res) => {
+    return 
 }
+
+
