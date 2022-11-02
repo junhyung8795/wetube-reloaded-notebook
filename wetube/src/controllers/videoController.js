0a341ab0a176732a03d4ece33bbb6a6c2a593013
junhyung8795 <junhyung8795@naver.com>
2022-11-02 16:48:31 +0900

    Array database(1)
    I just let the videos array out from trending(). This mean I can use the array at any other function. First to show the video when I click the llink, I just make the anchor("a") inside of title("h1"). See line+4.
    There is javascript code `${}`. In html attribute part, we can't use #{}. One way is 
    "/videos/" + info.id
    Second way is `/videos/${info.id}`. 
    Last, we must change "see controller". By send the id that comes from request, see controller can find the video which user want to watch, using "video= videos[id-1].and then that "video"'s can be sended to pageTitle.

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,35 +1,42 @@
 
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments:2,
-            createdAt:"2 minutes ago",
-            views: 59,
-            id:1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments:2,
-            createdAt:"2 minutes ago",
-            views: 59,
-            id:1,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments:2,
-            createdAt:"2 minutes ago",
-            views: 59,
-            id:1,
-        },
-    ];
     return res.render("home", {pageTitle: "Home", videos})
 };
 
-export const see =(req, res) => res.render("watch");
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views: 59,
+        id:1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views: 59,
+        id:2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views: 59,
+        id:3,
+    },
+];
+
+export const see =(req, res) => {
+    const id = req.params.id;/*get의 request로부터 id를 받고*/
+    /*const { id }= req.param도 똑같은 의미이다.*/ 
+    const video = videos[id-1];/*id를 통해 video를 받는다.
+    라우터애서 id를 받고-> 그 아이디를 request정보로 받고 -> 그 id로 video가 어떤애인지 배열에서 찾고 저장하는것*/
+    return res.render("watch", {pageTitle: `Watching ${video.title}`});
+}
 export const edit =(req, res) => res.render("edit");
 
 export const search =(req, res) => res.send("Search");
