76ccbfa472a2cb98a7481c8fd0019b58f1d76873
junhyung8795 <junhyung8795@naver.com>
2022-11-10 20:17:35 +0900

    Create Account (2)

--- wetube/src/controllers/userController.js
+++ wetube/src/controllers/userController.js
@@ -1,7 +1,17 @@
+import User from "../models/User.js";
+
 export const getJoin = (req, res) => res.render("join", {pageTitle:"Join"});
-export const postJoin = (req,res) => {
+export const postJoin = async(req,res) => {
     console.log(req.body);
-    res.end();
+    const {email, username, password, location ,name} = req.body;
+    await User.create({
+        name,
+        email,
+        username,
+        password,
+        location,
+    })
+    return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
