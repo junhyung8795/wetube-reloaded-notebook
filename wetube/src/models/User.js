0551803d99d24576d7ff0e7a1eb3272b7a905a82
junhyung8795 <junhyung8795@naver.com>
2022-11-10 20:51:59 +0900

    Create Account (3)
    I used bcrypt to hash the password that users write and pre("save") middleware to save it as hashed one before the password got saved,
    First, I used terminal to write "npm i bcrypt".
    And I made the mongoose middleware function from line11+ to 17+ in User.js.

--- wetube/src/models/User.js
+++ wetube/src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -7,6 +8,13 @@ const userSchema = new mongoose.Schema({
     name:{type:String, required: true},
     location:String,
 });
+userSchema.pre("save", async function(){
+    console.log("Users password:", this.password);
+    this.password= await bcrypt.hash(this.password, 5);//this는 저장이라는 이벤트가 생겨나는 객체 "User"를 가리킨다. user가 form에 입력한 password-> this.password
+                                                       //그리고 5는 saltRound인데 해쉬하는회수를 나타냄, 뒤에 원래는 callback함수도오지만 
+                                                       //이 경우에는 promise를 써서 callback함수가 따로 없다.
+    console.log("Hashed password:", this.password);
+})
 
 const User = mongoose.model("User", userSchema);
 
