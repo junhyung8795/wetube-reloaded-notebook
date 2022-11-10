daa87e41be285b189e87a3b151b443334c1065f3
junhyung8795 <junhyung8795@naver.com>
2022-11-10 21:41:16 +0900

    Form Validation
    Making a error message if there is a same username or email in the DB. To make it  I just modify the userController. If the same username or email exist then join page with errorMessage will show up. Focus on the operator "$or:[{}]" in line7+. {username:req.body.username} can be written like {username}. And Password Confirmation was also added.

--- wetube/src/controllers/userController.js
+++ wetube/src/controllers/userController.js
@@ -3,7 +3,15 @@ import User from "../models/User.js";
 export const getJoin = (req, res) => res.render("join", {pageTitle:"Join"});
 export const postJoin = async(req,res) => {
     console.log(req.body);
-    const {email, username, password, location ,name} = req.body;
+    const {email, username, password, password2, location ,name} = req.body;
+    const exists = await User.exists({$or:[{username:req.body.username},{email}]});
+    const pageTitle = "Join";
+    if(password !== password2){
+        return res.render("join", {pageTitle, errorMessage:"Password confirmation does not match."});        
+    }
+    if(exists){
+        return res.render("join", {pageTitle, errorMessage:"This username or email is already taken."});
+    }// 만약 username이 이미 사용중이라면 return이되어 밑에있는 db에 Usermodel에 정보를 저장하는일은 없어진다.
     await User.create({
         name,
         email,
