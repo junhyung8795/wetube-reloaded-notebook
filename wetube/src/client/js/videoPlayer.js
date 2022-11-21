eb6c536556206c5111f0266455c7ddf153d79d29
junhyung8795 <junhyung8795@naver.com>
2022-11-21 15:15:46 +0900

    I just added the pause and play button before
    But this time I added time bar and by using 
    new Date(), toISOString(), and substring() I could format the time that saying only seconds to real timer.

--- wetube/src/client/js/videoPlayer.js
+++ wetube/src/client/js/videoPlayer.js
@@ -1,9 +1,13 @@
 const video = document.querySelector("video");
 const playBtn =document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const volume = document.getElementById("volume");
-const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
+
+let volumeValue = 0.5;
+video.volume= volumeValue;
 
 const handlePlayClick = () => {
     if(video.paused) {
@@ -12,7 +16,7 @@ const handlePlayClick = () => {
     else {
         video.pause();
     }
-    playBtn.innerText= video.paused ? "Pause" : "Play";
+    playBtn.innerText= video.paused ? "Play" : "Pause";
 }
 
 const handleMuteClick = () => {
@@ -23,8 +27,30 @@ const handleMuteClick = () => {
         video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : volumeValue;
+}
+
+const handleVolumeChange = (event) => {
+    const {target:{value},} = event;
+    if(video.muted){
+        muteBtn.innerText="Mute";
+        video.muted=false;
+    } 
+    volumeValue=value;
+    video.volume=value;
 }
 
 
+const handleCurrentTime = () => {
+    currentTime.innerText = Math.floor(video.currentTime);
+}
+
+const handleTotalTime = () => {
+    totalTime.innerText = Math.floor(video.duration);
+}
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleTotalTime);
+video.addEventListener("timeupdate", handleCurrentTime);
