3dbe20d9d182fe8042ee108413e8888c9ece9808
junhyung8795 <junhyung8795@naver.com>
2022-11-23 18:51:31 +0900

    Recording Video(1)

--- wetube/src/client/js/recorder.js
+++ wetube/src/client/js/recorder.js
@@ -1,8 +1,33 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.addEventListener("click", handleStart);
+    startBtn.removeEventListener("click", handleStop);
+}
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        console.log(event);
+        console.log(event.data);
+      };
+    console.log(recorder);
+    recorder.start();
+    console.log(recorder);
+    setTimeout(()=>{
+        recorder.stop();
+    }, 5000);
+};
+
+const init = async() => {
+    stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
@@ -10,4 +35,5 @@ const handleStart = async () => {
   video.play();
 };
 
+init();
 startBtn.addEventListener("click", handleStart);
