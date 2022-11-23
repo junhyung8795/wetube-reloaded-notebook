c2c409e23e945119dbc24175ae1393f6178c6319
junhyung8795 <junhyung8795@naver.com>
2022-11-23 16:35:47 +0900

    Make the videoController prettier using icons.

--- wetube/src/client/js/videoPlayer.js
+++ wetube/src/client/js/videoPlayer.js
@@ -1,15 +1,21 @@
 const video = document.querySelector("video");
 const playBtn =document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline")
 const videoContainer = document.getElementById("videoContainer");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
+const videoControls = document.getElementById("videoControls");
 
 let volumeValue = 0.5;
 video.volume= volumeValue;
+let controlsTimeout= null;
+let controlsMovementTimeout = null;
 
 const handlePlayClick = () => {
     if(video.paused) {
@@ -18,7 +24,7 @@ const handlePlayClick = () => {
     else {
         video.pause();
     }
-    playBtn.innerText= video.paused ? "Play" : "Pause";
+    playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 }
 
 const handleMuteClick = () => {
@@ -28,7 +34,9 @@ const handleMuteClick = () => {
     else {
         video.muted = true;
     }
-    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
     volumeRange.value = video.muted ? 0 : volumeValue;
 }
 
@@ -62,12 +70,34 @@ const handleTimelineChange = (event) => {
 const handleFullScreenBtn = () => {
     const fullScreen = document.fullscreenElement;
     if(fullScreen) {
-        fullScreenBtn.innerText = "Full Screen";
-        document.exitFullscreen();
+        document.exitFullscreen();//풀스크린을 해제하는건 document에서 
+        fullScreenIcon.classList = "fas fa-expand";
     } else {
-        fullScreenBtn.innerText= "Exit Full Screen";
-        videoContainer.requestFullscreen();
+        videoContainer.requestFullscreen();//풀스크린을 적용하는건 htmlelement에서 하는 차이가있다.
+        fullScreenIcon.classList = "fas fa-compress";
     }
+}
+
+const hideControls = () => videoControls.classList.remove("showing");
+
+const handleMouseMove = () => {
+    if(controlsTimeout) {
+        clearTimeout(controlsTimeout);
+        controlsTimeout= null;
+    }
+
+    if(controlsMovementTimeout) {
+        clearTimeout(controlsMovementTimeout)
+        controlsMovementTimeout = null;
+    }
+    controlsMovementTimeout = setTimeout(hideControls, 3000);
+    videoControls.classList.add("showing");
+}
+
+const handleMouseLeave = () => {
+    controlsTimeout = setTimeout(hideControls, 3000);//setTimeout은 특정 id(int값)값을 반환하는데 이 아이디를 clearTimeout을 쓰면 cacel할수있다.
+    //이때 이 id는 cacel되고나면 사라지고 다음에 setTimeout을 또 실행해도 새로운 id값을 반환하기때문에 취소하면 null로 초기화시키고 다시 id값을 받아와
+    //clearTimeout을 해줘야 cacel할 수 있다. setTimeout은 시행할때마다 매번 다른 id값을 return한다는걸 기억해야함.
 }
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -77,3 +107,5 @@ video.readyState ? handleMetaData() : video.addEventListener("loadedmetadata", h
 video.addEventListener("timeupdate", handleCurrentTime);
 timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullScreenBtn);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
