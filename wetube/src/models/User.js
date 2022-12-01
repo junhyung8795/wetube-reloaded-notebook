d5fe9de4e33bde5c0eddedc12feb6c7ef9edabde
junhyung8795 <junhyung8795@naver.com>
2022-12-01 19:30:19 +0900

    Bug fix (isModified)

--- wetube/src/models/User.js
+++ wetube/src/models/User.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
 });
 userSchema.pre("save", async function () {
-    if (this.isModified()) {
+    if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 5);
     }
 });
