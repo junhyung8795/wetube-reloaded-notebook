0104c14b165e8ae7d9f0effa7a6fcb657c5a61fb
junhyung8795 <junhyung8795@naver.com>
2022-12-01 23:02:48 +0900

    Register View Event(dataset, data-anynameofyouwant, element.dataset.anynameofyouwant))

--- wetube/src/client/js/commentSection.js
+++ wetube/src/client/js/commentSection.js
@@ -6,7 +6,7 @@ const btn = form.querySelector("button");
 const handleSubmit = (event) => {
     event.preventDefault();
     const text = textarea.value;
-    const video = videoContainer.dataset.id; //watch에서 videoContainer를 보면 data-id=video._id를 볼 수 있는데 videoContainer.dataset.id를 하면 data-"id"의 정보를 가져올 수 있다.
+    const videoId = videoContainer.dataset.id; //watch에서 videoContainer를 보면 data-id=video._id를 볼 수 있는데 videoContainer.dataset.id를 하면 data-"id"의 정보를 가져올 수 있다.
 };
 
 form.addEventListener("submit", handleSubmit);
